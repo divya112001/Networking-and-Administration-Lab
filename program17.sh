7ccc254c588a74020ab471bdc25e8ef2d6d0eb14
Divya.m.k <116252848+divya112001@users.noreply.github.com>
2023-07-14 10:10:06 +0000

    Update program17.sh
    REVERSE

--- program17.sh
+++ program17.sh
@@ -17,3 +17,28 @@ mv temp file
 done
 cat rfile
 exit 0
+
+
+OUTPUT
+
+ksb@ksb-H410M-S2-V2:~/scripts$ sh program17.sh program17.sh
+exit 0
+cat rfile
+done
+mv temp file
+head -n $l file > temp
+l=`expr $l - 1`
+tail -n 1 file >> rfile
+do
+while [ $l -ge 1 ] 
+rm rfile
+l=`cat file | wc -l`
+cp $1 file
+fi 
+exit 1
+echo "syntax is <$0> <filename>"
+then
+if [ $# -ne 1 ]
+#set -xv
+#to reverse a file
+
